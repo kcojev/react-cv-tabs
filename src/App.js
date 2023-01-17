85ab829e39e301130ee1f0f0ff72b6132f7daf36
Georgy Kcojev <kcojev@gmail.com>
2023-01-17 11:43:40 +0300

    display job item

--- src/App.js
+++ src/App.js
@@ -29,8 +29,33 @@ function App() {
     )
   }
 
+  const {company, dates, duties, title} = jobs[value]
 
-  return <h2>jobs</h2>
+  return (
+    <section className='section'>
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-dates">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
